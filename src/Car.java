88adac21a2128a351b08049b4868af3b320a9522
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-12-17 23:24:27 +0300

    Task 1

--- src/Car.java
+++ src/Car.java
@@ -1,1 +1,22 @@
+public class Car {
+    private String brand;
+    private String model;
+    private float engineVolume;
+    private String colour;
+    private int year;
+    private String country;
 
+    Car (String brand, String model, float engineVolume, String colour, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.colour = colour;
+        this.year = year;
+        this.country = country;
+
+    }
+    public String toString() {
+        return brand + model + engineVolume + colour + year + country;
+    }
+
+}
