ad52ca6f42e682b9d1dd866448fa0c482b4d2b11
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-12-18 02:19:28 +0300

    Task 3

--- src/Car.java
+++ src/Car.java
@@ -1,22 +1,36 @@
 public class Car {
-    private final String brand;
-    private final String model;
-    private final float engineVolume;
-    private final String colour;
-    private final int year;
-    private final String country;
+    String brand;
+    String model;
+    float engineVolume;
+    String colour;
+    int year;
+    String country;
 
-    Car (String brand, String model, float engineVolume, String colour, int year, String country) {
+    Car(String brand, String model, float engineVolume, String colour, int year, String country) {
         this.brand = brand;
         this.model = model;
         this.engineVolume = engineVolume;
         this.colour = colour;
         this.year = year;
         this.country = country;
-
     }
+
+    @Override
     public String toString() {
-        return brand + model + engineVolume + colour + year + country;
+        if (brand == null || brand.isEmpty()) {
+            brand = "Default";
+        } if (model == null || model.isEmpty()) {
+            model = "Default";
+        } if (country == null || country.isEmpty()) {
+            country = "Default";
+        } if (year <= 0) {
+            year = 2000;
+        } if (colour == null || colour.isEmpty()) {
+            colour = "White";
+        } if (engineVolume <= 0) {
+            engineVolume = 1.5f;
+        }
+        return (brand + model + engineVolume + colour + year + country);
     }
-
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
     public static void main (String[] args) {
         Car car1 = new Car("Lada", " Granta ", 1.7f, " Yellow ", 2015, " Assembled in Russia");
         Car car2 = new Car("Audi", " A8 L TDI quattro ", 3.0f, " Black ", 2020, " Assembled in Germany");
         Car car3 = new Car("BMW", " Z8 ", 3.0f, " Black ", 2021, " Assembled in Germany");
         Car car4 = new Car("Kia", " Sportage 4-th Gen ", 2.4f, " Red ", 2018, " Assembled in South Korea");
-        Car car5 = new Car("Hyundai", " Avante ", 1.6f, " Orange ", 2016, " Assembled in South Korea");
+        Car car5 = new Car("", " Avente ", 0, " Orange ", 0, " Assembled in South Korea");
         System.out.println(car1);
         System.out.println(car2);
         System.out.println(car3);
         System.out.println(car4);
         System.out.println(car5);
+
     }
 }
